d3087381e6567fc946ab41aa26f873cd5a9957e8
Dallibo <denismalyarenko1@gmail.com>
2023-07-09 20:28:34 +0300

    Hw

--- Hw_js_16.js
+++ Hw_js_16.js
@@ -29,3 +29,48 @@ let refilMarker = new RefilMarker ('red', 30)
 refilMarker.print('Im refil marker')
 refilMarker.fill(50)
 refilMarker.print('Hellow my class')
+
+
+class ExtendedDate extends Date {
+    constructor(year, month, day) {
+      super(year, month, day);
+    }
+  
+    formatDate() {
+      const day = this.getDate();
+      const month = this.getMonth();
+      const monthNames = [
+        "січня", "лютого", "березня", "квітня", "травня", "червня",
+        "липня", "серпня", "вересня", "жовтня", "листопада", "грудня"
+      ];
+      const monthName = monthNames[month];
+      return `${day} ${monthName}`;
+    }
+  
+    currentDate() {
+      const currentDate = new Date();
+      return this >= currentDate;
+    }
+    
+    isLeap(){
+        const year1 = this.getFullYear()
+        return (year1 % 4 === 0 && year1 % 100 !== 0) || year1 % 400 === 0;
+    }
+
+    nextDate() {
+        const nextDate = customDate;
+        nextDate.setDate(nextDate.getDate() + 1);
+        return nextDate;
+      }
+  }
+  
+  const customDate = new ExtendedDate(2023, 6, 31);
+  console.log(customDate.formatDate());
+  console.log(customDate.currentDate());
+  console.log(customDate.isLeap());
+  console.log(customDate.nextDate());
+
+
+
+
+
